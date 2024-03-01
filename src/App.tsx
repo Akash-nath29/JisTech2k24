78228ee13cf3036c31f66e723b04affe4fb17396
subh05sus <sahasubhadipfkk@gmail.com>
2024-03-02 01:49:02 +0530

    lag reduction for hero section

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,24 @@
-import Hero from "./sections/Hero"
-import RemainingPart from "./sections/remainingPart"
+import { useState, useEffect } from 'react';
+import Hero from './sections/Hero';
+import RemainingPart from './sections/remainingPart';
 
 function App() {
+  const [heroKey, setHeroKey] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setHeroKey(prevKey => prevKey + 1);
+    }, 5000);
+
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <>
-    <Hero/>
-    <RemainingPart/>
+      <Hero key={heroKey} />
+      <RemainingPart />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
