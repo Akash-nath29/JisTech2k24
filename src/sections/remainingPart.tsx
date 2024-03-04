2b74b6ba83072454c87d7af2d010c5a4fe1f5bbd
Subhadip Saha <sahasubhadipfkk@gmail.com>
2024-03-04 14:50:07 +0530

    Update remainingPart.tsx

--- src/sections/remainingPart.tsx
+++ src/sections/remainingPart.tsx
@@ -3,7 +3,7 @@ import Timeline from "./Timeline";
 
 function remainingPart() {
   return (
-    <section className="flex items-center justify-center landscape:p-10 w-full h-[200vh]" id="content">
+    <section className="flex items-center justify-center landscape:p-10 w-full h-auto" id="content">
       <div className=" landscape:rounded-3xl bg-gray-800 h-full w-full p-10">
         <Events/>
         <Timeline/>
